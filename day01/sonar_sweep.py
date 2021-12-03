a8c0c396b5b9a61a08544d7b317624d6c2b51ba7
Gellert M <mgellert@users.noreply.github.com>
2021-12-03 14:09:33 +0100

    Solved day 1 part 2.

--- day01/sonar_sweep.py
+++ day01/sonar_sweep.py
@@ -7,4 +7,9 @@ def read_file_to_int(name: str) -> List[int]:
 
 
 def count_larger_measurements(measurements: List[int]) -> int:
-    return sum([1 for a, b in list(zip(measurements[:-1], measurements[1:])) if b > a])
+    return sum([1 for a, b in list(zip(measurements, measurements[1:])) if b > a])
+
+
+def count_larger_sums(measurements: List[int]) -> int:
+    triplets = list(zip(measurements, measurements[1:], measurements[2:]))
+    return sum([1 for a, b in list(zip(triplets, triplets[1:])) if sum(b) > sum(a)])
--- day01/sonar_sweep_test.py
+++ day01/sonar_sweep_test.py
@@ -2,7 +2,7 @@ import unittest
 from dataclasses import dataclass
 from typing import List
 
-from day01.sonar_sweep import count_larger_measurements, read_file_to_int
+from day01.sonar_sweep import count_larger_measurements, read_file_to_int, count_larger_sums
 
 
 class SonarSweepTest(unittest.TestCase):
@@ -19,6 +19,21 @@ class SonarSweepTest(unittest.TestCase):
 
         for case in testcases:
             actual = count_larger_measurements(case.input)
+            self.assertEqual(case.expected, actual)
+
+    def test_count_larger_sums(self):
+        @dataclass
+        class TestCase:
+            input: List[int]
+            expected: int
+
+        testcases = [
+            TestCase(input=[199, 200, 208, 210, 200, 207, 240, 269, 260, 263], expected=5),
+            TestCase(input=read_file_to_int("day01"), expected=1858)
+        ]
+
+        for case in testcases:
+            actual = count_larger_sums(case.input)
             self.assertEqual(case.expected, actual)
 
 
