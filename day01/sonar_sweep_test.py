a50e3d90e93745effe82cbdcdab6873bf9028396
Gellert M <mgellert@users.noreply.github.com>
2021-12-03 13:29:12 +0100

    Initial commit. Solved day 1 part 1.

---
+++ day01/sonar_sweep_test.py
@@ -0,0 +1,26 @@
+import unittest
+from dataclasses import dataclass
+from typing import List
+
+from day01.sonar_sweep import count_larger_measurements, read_file_to_int
+
+
+class SonarSweepTest(unittest.TestCase):
+    def test_count_larger_measurements(self):
+        @dataclass
+        class TestCase:
+            input: List[int]
+            expected: int
+
+        testcases = [
+            TestCase(input=[199, 200, 208, 210, 200, 207, 240, 269, 260, 263], expected=7),
+            TestCase(input=read_file_to_int("day01"), expected=1832)
+        ]
+
+        for case in testcases:
+            actual = count_larger_measurements(case.input)
+            self.assertEqual(case.expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
