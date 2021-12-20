6bc91a220a067fdb05b8df14776b34a14e6e0d46
Gellert M <mgellert@users.noreply.github.com>
2021-12-20 09:29:50 +0100

    Solved day 16 part 2.

--- day16/packet_decoder_test.py
+++ day16/packet_decoder_test.py
@@ -1,7 +1,7 @@
 import unittest
 from dataclasses import dataclass
 
-from day16.packet_decoder import convert_to_bin, read_input, sum_packet_versions
+from day16.packet_decoder import convert_to_bin, read_input, sum_packet_versions, calculate_packet_value
 
 
 @dataclass
@@ -20,11 +20,29 @@ class PacketDecoderTest(unittest.TestCase):
             TestCase(input="620080001611562C8802118E34", expected=12),
             TestCase(input="C0015000016115A2E0802F182340", expected=23),
             TestCase(input="A0016C880162017C3686B18A3D4780", expected=31),
-            TestCase(input=read_input(), expected=1002),
+            TestCase(input=read_input(), expected=1002)
         ]
 
         for case in testcases:
             actual = sum_packet_versions(case.input)
+            self.assertEqual(case.expected, actual)
+
+    def test_calculate_packet_value(self):
+        testcases = [
+            TestCase(input="D2FE28", expected=2021),
+            TestCase(input="C200B40A82", expected=3),
+            TestCase(input="04005AC33890", expected=54),
+            TestCase(input="880086C3E88112", expected=7),
+            TestCase(input="CE00C43D881120", expected=9),
+            TestCase(input="D8005AC2A8F0", expected=1),
+            TestCase(input="F600BC2D8F", expected=0),
+            TestCase(input="9C005AC2F8F0", expected=0),
+            TestCase(input="9C0141080250320F1802104A08", expected=1),
+            TestCase(input=read_input(), expected=1673210814091)
+        ]
+
+        for case in testcases:
+            actual = calculate_packet_value(case.input)
             self.assertEqual(case.expected, actual)
 
 
