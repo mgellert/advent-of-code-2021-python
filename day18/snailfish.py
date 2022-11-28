b47920f939d049f53235b8a90bfd31b6732afa83
Gellert M <mgellert@users.noreply.github.com>
2022-11-28 20:18:08 +0100

    Status saved.

--- day18/snailfish.py
+++ day18/snailfish.py
@@ -1,135 +1,63 @@
 import math
-from dataclasses import dataclass
 from typing import Tuple, Type, List
 
 
-class Node:
-    def __init__(self, left=None, right=None):
-        self.left = left
-        self.right = right
+def parse(raw: str) -> List[any]:
+    a = []
+    a.append(eval(raw))
+    return a
 
-    def __eq__(self, o: object) -> bool:
-        if isinstance(o, Node):
-            return self.left == o.left and self.right == o.right
+
+def _search_and_add_right(number: List[any], exploded_node, value_to_add, depth=0):
+    if type(number) is int:
         return False
-
-    def __str__(self):
-        return f"Node(left={self.left}, right={self.right})"
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-
-def _parse(input) -> Node:
-    node = Node()
-
-    if type(input[0]) is int:
-        left = input[0]
-    else:
-        left = _parse(input[0])
-
-    if type(input[1]) is int:
-        right = input[1]
-    else:
-        right = _parse(input[1])
-
-    node.left = left
-    node.right = right
-    return node
+    right = number[1]
+    if type(right) is int:
+        number[1] += value_to_add
+        return True
+    elif type(right) is list and right != exploded_node:
+        return _search_and_add_right(number[0], exploded_node, value_to_add, depth + 1)
 
 
-def parse(raw: str) -> Node:
-    return _parse(eval(raw))
-
-
-def _to_list(node, list):
-    if type(node) is int:
-        return list
-    if node.left is not None:
-        _to_list(node.left, list)
-
-    if type(node.left) is int or type(node.right) is int:
-        list.append(node)
-
-    if node.right is not None:
-        _to_list(node.right, list)
-    return list
-
-
-def _add_to_right(list: List[any], value, i):
-    if i < len(list):
-        node = list[i]
-        if type(node.right) is int:
-            node.right += value
-        elif type(node.left) is int:
-            node.left += value
-
-
-def _add_to_left(list: List[any], value, i):
-    if i > 0:
-        node = list[i - 1]
-        if type(node.left) is int:
-            node.left += value
-        elif type(node.right) is int:
-            node.right += value
-
-
-def _explode(node, as_list, depth=1, done=False):
-    if type(node) is int:
-        return None, None, None, None
-    if depth == 4 and not done:
-        if type(node.left) is Node:
-            temp = node.left
-            node.left = 0
-            return temp.left, temp.right, as_list.index(temp), True
-        elif type(node.right) is Node:
-            temp = node.right
-            node.right = 0
-            return temp.left, temp.right, as_list.index(temp), True
-    left, right, i, done = _explode(node.left, as_list, depth + 1, done)
-    if left is not None and right is not None:
-        return left, right, i, done
-    left, right, i, done = _explode(node.right, as_list, depth + 1, done)
-    if left is not None and right is not None:
-        return left, right, i, done
-
-    return None, None, None, None
-
-
-def explode(node):
-    left, right, i, done = _explode(node, _to_list(node, []))
-    if not done:
+def _search_and_add_left(number: List[any], exploded_node, value_to_add):
+    if type(number) is int:
         return False
-    _add_to_right(_to_list(node, []), right, i)
-    _add_to_left(_to_list(node, []), left, i)
-    return True
+    left = number[0]
+    if type(left) is int:
+        number[0] += value_to_add
+        return True
+    elif type(left) is list and left != exploded_node:
+        return _search_and_add_left(left, exploded_node, value_to_add)
 
 
-def split(node, done=False):
-    if type(node.left) is int and not done:
-        if node.left >= 10:
-            value = node.left
-            node.left = Node(left=math.floor(value / 2), right=math.ceil(value / 2))
-            return True
-    if type(node.right) is int and not done:
-        if node.right >= 10:
-            value = node.right
-            node.right = Node(left=math.floor(value / 2), right=math.ceil(value / 2))
-            return True
-    if type(node.left) is Node:
-        done = split(node.left, done)
-    if type(node.right) is Node:
-        done = split(node.right, done)
-    return done
+def explode(number: List[any], depth: int = 0, exploded_once: bool = False):
+    left, right, exploded_node = None, None, None
+    for i in number:
+        if depth == 3 and type(i) is list and not exploded_once:
+            if type(i[0]) is list:
+                left, right = i[0]
+                i[0] = 0
+                i[1] += right
+                return left, None, i, True
+            if type(i[1]) is list:
+                left, right = i[1]
+                i[1] = 0
+                i[0] += left
+                return None, right, i, True
+
+        if type(i) is not int and not exploded_node:
+            left, right, exploded_node, exploded_once = explode(i, depth + 1, exploded_once)
+        if right is not None:
+            added = _search_and_add_right(i, exploded_node, right)
+            if added:
+                return None, None, None, exploded_once
+        if left is not None:
+            added = _search_and_add_left(i, exploded_node, left)
+            if added:
+                return None, None, None, exploded_once
+    return left, right, exploded_node, exploded_once
 
 
-def reduce(node):
-    exploded = True
-    splitted = True
-    while exploded or splitted:
-        exploded = explode(node)
-        while exploded:
-            exploded = explode(node)
-            print("exploded")
-        splitted = split(node)
-        print("splitted")
+def reduce(raw: str):
+    number = parse(raw)
+    explode(number)
--- day18/snailfish_test.py
+++ day18/snailfish_test.py
@@ -1,74 +1,34 @@
 import unittest
 
-from day18.snailfish import explode, Node, parse, split, reduce
+from day18.snailfish import parse, explode
 
 
 class SnailfishTest(unittest.TestCase):
-    def test_parsing(self):
-        actual = parse("[1,2]")
-        expected = Node(left=1, right=2)
-        self.assertEqual(expected, actual)
-
-        actual = parse("[3,[1,2]]")
-        expected = Node(left=3, right=Node(left=1, right=2))
-        self.assertEqual(expected, actual)
-
     def test_explode(self):
-        actual = parse("[[[[[9,8],1],2],3],4]")
-        expected = parse("[[[[0,9],2],3],4]")
-        explode(actual)
-        self.assertEqual(expected, actual)
-
-        actual = parse("[7,[6,[5,[4,[3,2]]]]]")
-        expected = parse("[7,[6,[5,[7,0]]]]")
-        explode(actual)
-        self.assertEqual(expected, actual)
-
-        actual = parse("[[6,[5,[4,[3,2]]]],1]")
-        expected = parse("[[6,[5,[7,0]]],3]")
-        explode(actual)
-        self.assertEqual(expected, actual)
+        # actual = parse("[[[[[9,8],1],2],3],4]")
+        # expected = parse("[[[[0,9],2],3],4]")
+        # explode(actual)
+        # self.assertEqual(expected, actual)
+        #
+        # actual = parse("[7,[6,[5,[4,[3,2]]]]]")
+        # expected = parse("[7,[6,[5,[7,0]]]]")
+        # explode(actual)
+        # self.assertEqual(expected, actual)
+        #
+        # actual = parse("[[6,[5,[4,[3,2]]]],1]")
+        # expected = parse("[[6,[5,[7,0]]],3]")
+        # explode(actual)
+        # self.assertEqual(expected, actual)
 
         actual = parse("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")
         expected = parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
         explode(actual)
         self.assertEqual(expected, actual)
-
-        actual = parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
-        expected = parse("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
-        explode(actual)
-        self.assertEqual(expected, actual)
-
-    def test_split(self):
-        actual = parse("[1,10]")
-        expected = parse("[1,[5,5]]")
-        splitted = split(actual)
-        self.assertEqual(expected, actual)
-        self.assertTrue(splitted)
-
-        actual = parse("[1,11]")
-        expected = parse("[1,[5,6]]")
-        splitted = split(actual)
-        self.assertEqual(expected, actual)
-        self.assertTrue(splitted)
-
-        actual = parse("[10,10]")
-        expected = parse("[[5,5],10]")
-        splitted = split(actual)
-        self.assertEqual(expected, actual)
-        self.assertTrue(splitted)
-
-        actual = parse("[1,1]")
-        expected = parse("[1,1]")
-        splitted = split(actual)
-        self.assertEqual(expected, actual)
-        self.assertFalse(splitted)
-
-    def test_reduce(self):
-        actual = parse("[[[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]]")
-        expected = parse("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
-        reduce(actual)
-        self.assertEqual(expected, actual)
+        #
+        # actual = parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+        # expected = parse("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+        # explode(actual)
+        # self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
