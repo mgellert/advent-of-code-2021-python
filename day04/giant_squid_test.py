3a5b2044e9cf92503096a484e6d7fab8493fa808
gellertm <mgellert@users.noreply.github.com>
2023-08-04 07:55:21 +0200

    Refactored solution for day 4.

--- day04/giant_squid_test.py
+++ day04/giant_squid_test.py
@@ -1,10 +1,13 @@
 import textwrap
 import unittest
 
-from day04.giant_squid import BingoBoard, calculate_first_winner, calculate_last_winner
+from common import read_file
+from day04.giant_squid import BingoBoard, part_1, part_2, parse_input
 
 
 class TestGiantSquid(unittest.TestCase):
+    draws, boards = parse_input(read_file("day04"))
+
     def test_board_init(self):
         bingo = BingoBoard(textwrap.dedent("""
             22 13 17 11  0
@@ -29,20 +32,18 @@ class TestGiantSquid(unittest.TestCase):
                      2  0 12  3  7"""))
         draws = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21]
         for draw in draws:
-            res = bingo.mark(draw)
-            self.assertIsNone(res)
-        res = bingo.mark(24)
-        self.assertEqual(res, 4512)
+            bingo.mark(draw)
+            self.assertFalse(bingo.is_winner())
 
-    def test_calculate_first_winner(self):
-        got = calculate_first_winner()
-        expected = 39984
-        self.assertEqual(got, expected)
+        bingo.mark(24)
+        self.assertTrue(bingo.is_winner())
+        self.assertEqual(bingo.score() * 24, 4512)
 
-    def test_calculate_last_winner(self):
-        got = calculate_last_winner()
-        expected = 8468
-        self.assertEqual(got, expected)
+    def test_part_1_solution(self):
+        self.assertEqual(part_1(self.draws, self.boards), 39984)
+
+    def test_part_2_solution(self):
+        self.assertEqual(part_2(self.draws, self.boards), 8468)
 
 
 if __name__ == '__main__':
