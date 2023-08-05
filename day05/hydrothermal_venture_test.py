06a6490753f9946f906e3808c2eef195318ca7be
gellertm <mgellert@users.noreply.github.com>
2023-08-05 09:10:37 +0200

    Refactored solution for day 5.

--- day05/hydrothermal_venture_test.py
+++ day05/hydrothermal_venture_test.py
@@ -2,7 +2,8 @@ import unittest
 from dataclasses import dataclass
 from typing import List
 
-from day05.hydrothermal_venture import count_dangerous_areas, read_file
+from common import read_lines
+from day05.hydrothermal_venture import count_dangerous_areas, parse_lines
 
 
 @dataclass
@@ -12,45 +13,31 @@ class TestCase:
 
 
 class HydrothermalVentureTest(unittest.TestCase):
-    def test_count_dangerous_areas(self):
-        testcases = [
-            TestCase(input=[
-                "0,9 -> 5,9",
-                "8,0 -> 0,8",
-                "9,4 -> 3,4",
-                "2,2 -> 2,1",
-                "7,0 -> 7,4",
-                "6,4 -> 2,0",
-                "0,9 -> 2,9",
-                "3,4 -> 1,4",
-                "0,0 -> 8,8",
-                "5,5 -> 8,2"
-            ], expected=5),
-            TestCase(input=read_file(), expected=4826)
-        ]
-        for case in testcases:
-            actual = count_dangerous_areas(case.input)
-            self.assertEqual(case.expected, actual)
+    test_input = parse_lines((
+        "0,9 -> 5,9",
+        "8,0 -> 0,8",
+        "9,4 -> 3,4",
+        "2,2 -> 2,1",
+        "7,0 -> 7,4",
+        "6,4 -> 2,0",
+        "0,9 -> 2,9",
+        "3,4 -> 1,4",
+        "0,0 -> 8,8",
+        "5,5 -> 8,2",
+    ))
+    input = parse_lines(read_lines("day05"))
 
-    def test_count_dangerous_areas_with_diagonal(self):
-        testcases = [
-            TestCase(input=[
-                "0,9 -> 5,9",
-                "8,0 -> 0,8",
-                "9,4 -> 3,4",
-                "2,2 -> 2,1",
-                "7,0 -> 7,4",
-                "6,4 -> 2,0",
-                "0,9 -> 2,9",
-                "3,4 -> 1,4",
-                "0,0 -> 8,8",
-                "5,5 -> 8,2"
-            ], expected=12),
-            TestCase(input=read_file(), expected=16793)
-        ]
-        for case in testcases:
-            actual = count_dangerous_areas(case.input, diagonal=True)
-            self.assertEqual(case.expected, actual)
+    def test_part_1_example(self):
+        self.assertEqual(count_dangerous_areas(self.test_input), 5)
+
+    def test_part_1_solution(self):
+        self.assertEqual(count_dangerous_areas(self.input), 4826)
+
+    def test_part_2_example(self):
+        self.assertEqual(count_dangerous_areas(self.test_input, use_diagonals=True), 12)
+
+    def test_part_2_solution(self):
+        self.assertEqual(count_dangerous_areas(self.input, use_diagonals=True), 16793)
 
 
 if __name__ == '__main__':
