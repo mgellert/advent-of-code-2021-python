00b3c2ea59d08c7e62fa0efe3e52866b3a6f4bc1
gellertm <mgellert@users.noreply.github.com>
2023-08-15 21:17:14 +0200

    Refactored solution for day 11.

--- day11/dumbo_octopus.py
+++ day11/dumbo_octopus.py
@@ -1,13 +1,8 @@
 from typing import List
 
 
-def read_file() -> str:
-    with open("../inputs/day11", "r") as file:
-        return file.read()
-
-
-def _parse_input(input: str) -> List[List[int]]:
-    return [[int(char) for char in line] for line in input.split("\n")]
+def parse_input(raw: str) -> List[List[int]]:
+    return [[int(char) for char in line] for line in raw.split("\n")]
 
 
 def _will_flash(octopi: List[List[int]]) -> (int, int):
@@ -15,26 +10,23 @@ def _will_flash(octopi: List[List[int]]) -> (int, int):
         for j in range(0, len(octopi[i])):
             if octopi[i][j] > 9:
                 return i, j
-    return -1, -1
+    return None, None
 
 
 deltas = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
 
 def _step(octopi: List[List[int]]) -> (List[List[int]], int):
-    next_step: List[List[int]] = []
-    for i in range(0, len(octopi)):
-        next_step.append([])
-        for j in range(0, len(octopi[i])):
-            next_step[i].append(0)
+    next_step: List[List[int]] = [[0] * len(octopi[i]) for i in range(len(octopi))]
 
-    flashes = 0
+    # increase energy level
     for i in range(0, len(octopi)):
         for j in range(0, len(octopi[i])):
             next_step[i][j] = octopi[i][j] + 1
 
+    flashes = 0
     x, y = _will_flash(next_step)
-    while x > -1:
+    while x is not None:
         next_step[x][y] = 0
         flashes += 1
         for delta in deltas:
@@ -47,8 +39,7 @@ def _step(octopi: List[List[int]]) -> (List[List[int]], int):
     return next_step, flashes
 
 
-def count_flashes(input: str, steps: int = 100) -> int:
-    octopi = _parse_input(input)
+def count_flashes(octopi: List[List[int]], steps: int = 100) -> int:
     total = 0
     for _ in range(0, steps):
         octopi, flashes = _step(octopi)
@@ -64,12 +55,10 @@ def _is_all_zero(octopi: List[List[int]]) -> bool:
     return True
 
 
-def count_sync(input: str) -> int:
-    octopi = _parse_input(input)
+def count_sync(octopi: List[List[int]]) -> int:
     steps = 0
     while True:
         octopi, _ = _step(octopi)
         steps += 1
         if _is_all_zero(octopi):
-            break
-    return steps
+            return steps
--- day11/dumbo_octopus_test.py
+++ day11/dumbo_octopus_test.py
@@ -1,20 +1,12 @@
 import textwrap
 import unittest
-from dataclasses import dataclass
 
-from day11.dumbo_octopus import count_flashes, read_file, count_sync
-
-
-@dataclass
-class TestCase:
-    input: str
-    expected: int
+from common import read_file
+from day11.dumbo_octopus import count_flashes, count_sync, parse_input
 
 
 class DumboOctopusTest(unittest.TestCase):
-    def test_count_flashes(self):
-        testcases = [
-            TestCase(input=textwrap.dedent("""
+    test_input = parse_input(textwrap.dedent("""
             5483143223
             2745854711
             5264556173
@@ -25,32 +17,20 @@ class DumboOctopusTest(unittest.TestCase):
             6882881134
             4846848554
             5283751526
-            """).strip(), expected=1656),
-            TestCase(input=read_file(), expected=1749)
-        ]
-        for case in testcases:
-            actual = count_flashes(case.input)
-            self.assertEqual(case.expected, actual)
+            """).strip())
+    input = parse_input(read_file("day11"))
 
-    def test_count_sync(self):
-        testcases = [
-            TestCase(input=textwrap.dedent("""
-            5483143223
-            2745854711
-            5264556173
-            6141336146
-            6357385478
-            4167524645
-            2176841721
-            6882881134
-            4846848554
-            5283751526
-            """).strip(), expected=195),
-            TestCase(input=read_file(), expected=285)
-        ]
-        for case in testcases:
-            actual = count_sync(case.input)
-            self.assertEqual(case.expected, actual)
+    def test_part_1_example(self):
+        self.assertEqual(count_flashes(self.test_input), 1656)
+
+    def test_part_1_solution(self):
+        self.assertEqual(count_flashes(self.input), 1749)
+
+    def test_part_2_example(self):
+        self.assertEqual(count_sync(self.test_input), 195)
+
+    def test_part_2_solution(self):
+        self.assertEqual(count_sync(self.input), 285)
 
 
 if __name__ == '__main__':
