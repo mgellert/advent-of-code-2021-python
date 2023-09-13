055b9fc16068b73a9c2cdf7269b4c347de4d4f6f
gellertm <mgellert@users.noreply.github.com>
2023-09-13 08:45:02 +0200

    Refactored solution for day 13.

--- day13/transparent_origami_test.py
+++ day13/transparent_origami_test.py
@@ -1,79 +1,47 @@
 import unittest
-from dataclasses import dataclass
-from typing import List
 
-from day13.transparent_origami import visible_dots_after_first_fold, read_input, fold_all
-
-
-@dataclass
-class TestCase:
-    input: List[str]
-    expected: int
+from common import read_lines
+from day13.transparent_origami import visible_dots_after_first_fold, fold_all
 
 
 class TransparentOrigamiTest(unittest.TestCase):
-    def test_visible_dots_after_first_fold(self):
-        testcases = [
-            TestCase(input=[
-                "6,10",
-                "0,14",
-                "9,10",
-                "0,3",
-                "10,4",
-                "4,11",
-                "6,0",
-                "6,12",
-                "4,1",
-                "0,13",
-                "10,12",
-                "3,4",
-                "3,0",
-                "8,4",
-                "1,10",
-                "2,14",
-                "8,10",
-                "9,0",
-                "",
-                "fold along y=7",
-                "fold along x=5"
-            ], expected=17),
-            TestCase(input=read_input(), expected=1)
-        ]
+    test_input = [
+        "6,10",
+        "0,14",
+        "9,10",
+        "0,3",
+        "10,4",
+        "4,11",
+        "6,0",
+        "6,12",
+        "4,1",
+        "0,13",
+        "10,12",
+        "3,4",
+        "3,0",
+        "8,4",
+        "1,10",
+        "2,14",
+        "8,10",
+        "9,0",
+        "",
+        "fold along y=7",
+        "fold along x=5"]
+    input = read_lines("day13")
 
-        for case in testcases:
-            actual = visible_dots_after_first_fold(case.input)
-            self.assertEqual(case.expected, actual)
+    def test_part_1_example(self):
+        self.assertEqual(visible_dots_after_first_fold(self.test_input), 17)
 
-    def test_fold_all(self):
-        testcases = [
-            TestCase(input=[
-                "6,10",
-                "0,14",
-                "9,10",
-                "0,3",
-                "10,4",
-                "4,11",
-                "6,0",
-                "6,12",
-                "4,1",
-                "0,13",
-                "10,12",
-                "3,4",
-                "3,0",
-                "8,4",
-                "1,10",
-                "2,14",
-                "8,10",
-                "9,0",
-                "",
-                "fold along y=7",
-                "fold along x=5"
-            ], expected=17),
-            TestCase(input=read_input(), expected=1)
-        ]
+    def test_part_1_solution(self):
+        self.assertEqual(visible_dots_after_first_fold(self.input), 847)
 
-        for case in testcases:
-            fold_all(case.input)
+    def test_part_2_example(self):
+        print("Day 13 Part 2 Test:")
+        print(fold_all(self.test_input))
+
+    def test_part_2_solution(self):
+        print("Day 13 Part 2 Solution:")
+        print(fold_all(self.input))
 
 
 if __name__ == '__main__':
