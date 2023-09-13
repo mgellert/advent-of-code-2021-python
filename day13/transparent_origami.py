9446d8ae48e20614c06000db07b5dd9c2ec09c4e
gellertm <mgellert@users.noreply.github.com>
2023-09-13 08:45:02 +0200

    Refactored solution for day 13.

--- day13/transparent_origami.py
+++ day13/transparent_origami.py
@@ -6,81 +6,84 @@ dot_re = re.compile("(\\d+),(\\d+)")
 fold_re = re.compile("fold along ([xy])=(\\d+)")
 
 
-def read_input() -> List[str]:
-    with open("../inputs/day13", "r") as file:
-        return file.readlines()
-
-
 @dataclass(frozen=True)
-class Point:
+class Dot:
     x: int
     y: int
 
-    def __lt__(self, other: Type["Point"]):
+    def __lt__(self, other: Type["Dot"]):
         if self.y != other.y:
             return self.y < other.y
         else:
             return self.x < other.x
 
 
-def _parse_dots(lines: List[str]) -> Set[Point]:
+@dataclass(frozen=True)
+class Fold:
+    direction: str
+    position: int
+
+
+def _parse_dots(lines: List[str]) -> Set[Dot]:
     dots = set()
     for line in lines:
         m = dot_re.match(line)
         if m:
-            dots.add(Point(x=int(m[1]), y=int(m[2])))
+            dots.add(Dot(x=int(m[1]), y=int(m[2])))
     return dots
 
 
-def _parse_folds(lines: List[str]) -> List[Tuple[str, int]]:
+def _parse_folds(lines: List[str]) -> List[Fold]:
     folds = []
     for line in lines:
         m = fold_re.match(line)
         if m:
-            folds.append((m[1], int(m[2])))
+            folds.append(Fold(m[1], int(m[2])))
     return folds
 
 
-def _fold(dots: Set[Point], dir: str, pos: int) -> Set[Point]:
+def _fold(dots: Set[Dot], fold: Fold) -> Set[Dot]:
     folded = set()
-    if dir == 'y':
+    if fold.direction == 'y':
         for dot in dots:
-            if dot.y < pos:
+            if dot.y < fold.position:
                 folded.add(dot)
-            elif dot.y > pos:
-                folded.add(Point(y=2 * pos - dot.y, x=dot.x))
-    elif dir == 'x':
+            elif dot.y > fold.position:
+                folded.add(Dot(y=2 * fold.position - dot.y, x=dot.x))
+    elif fold.direction == 'x':
         for dot in dots:
-            if dot.x < pos:
+            if dot.x < fold.position:
                 folded.add(dot)
-            elif dot.x > pos:
-                folded.add(Point(x=2 * pos - dot.x, y=dot.y))
+            elif dot.x > fold.position:
+                folded.add(Dot(x=2 * fold.position - dot.x, y=dot.y))
     return folded
 
 
-def _print(dots: Set[Point]):
-    max_x = max([d.x for d in dots])
-    max_y = max([d.y for d in dots])
+def _print(dots: Set[Dot]) -> str:
+    max_x = max(d.x for d in dots)
+    max_y = max(d.y for d in dots)
 
+    paper = ""
     for i in range(0, max_y + 1):
         for j in range(0, max_x + 1):
-            char = "#" if Point(y=i, x=j) in dots else " "
-            print(char, end="")
-        print()
+            char = "#" if Dot(y=i, x=j) in dots else " "
+            paper += char
+        paper += "\n"
+    return paper
 
 
 def visible_dots_after_first_fold(lines: List[str]) -> int:
     dots = _parse_dots(lines)
     folds = _parse_folds(lines)
 
-    folded = _fold(dots, *folds[0])
+    folded = _fold(dots, folds[0])
     return len(folded)
 
 
-def fold_all(lines: List[str]):
+def fold_all(lines: List[str]) -> str:
     dots = _parse_dots(lines)
     folds = _parse_folds(lines)
 
     for fold in folds:
-        dots = _fold(dots, *fold)
-    _print(dots)
+        dots = _fold(dots, fold)
+    return _print(dots)
