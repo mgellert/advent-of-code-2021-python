fc6650cdd27b1f8fd2d799abff447acf532cbb14
gellertm <mgellert@users.noreply.github.com>
2023-09-19 11:28:34 +0200

    Solved day 18 part 1 and 2.

--- day18/snailfish.py
+++ day18/snailfish.py
@@ -1,63 +1,155 @@
-import math
-from typing import Tuple, Type, List
+class Wrapper:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def __repr__(self) -> str:
+        return str(self.value)
+
+    def add(self, other):
+        self.value += other.value
+
+    def split(self):
+        new_value = self.value // 2
+        return Wrapper(new_value), Wrapper(new_value + (self.value % 2))
 
 
-def parse(raw: str) -> List[any]:
-    a = []
-    a.append(eval(raw))
-    return a
+def parse(raw):
+    return eval(raw)
 
 
-def _search_and_add_right(number: List[any], exploded_node, value_to_add, depth=0):
-    if type(number) is int:
-        return False
-    right = number[1]
-    if type(right) is int:
-        number[1] += value_to_add
-        return True
-    elif type(right) is list and right != exploded_node:
-        return _search_and_add_right(number[0], exploded_node, value_to_add, depth + 1)
+def wrap(number):
+    for idx, n in enumerate(number):
+        if type(n) == int:
+            number[idx] = Wrapper(n)
+        else:
+            wrap(n)
+    return number
 
 
-def _search_and_add_left(number: List[any], exploded_node, value_to_add):
-    if type(number) is int:
-        return False
-    left = number[0]
-    if type(left) is int:
-        number[0] += value_to_add
-        return True
-    elif type(left) is list and left != exploded_node:
-        return _search_and_add_left(left, exploded_node, value_to_add)
+def unwrap(number):
+    if number is None:
+        return
+    for idx, n in enumerate(number):
+        if type(n) == Wrapper:
+            number[idx] = n.value
+        else:
+            unwrap(n)
+    return number
 
 
-def explode(number: List[any], depth: int = 0, exploded_once: bool = False):
-    left, right, exploded_node = None, None, None
-    for i in number:
-        if depth == 3 and type(i) is list and not exploded_once:
-            if type(i[0]) is list:
-                left, right = i[0]
-                i[0] = 0
-                i[1] += right
-                return left, None, i, True
-            if type(i[1]) is list:
-                left, right = i[1]
-                i[1] = 0
-                i[0] += left
-                return None, right, i, True
-
-        if type(i) is not int and not exploded_node:
-            left, right, exploded_node, exploded_once = explode(i, depth + 1, exploded_once)
-        if right is not None:
-            added = _search_and_add_right(i, exploded_node, right)
-            if added:
-                return None, None, None, exploded_once
-        if left is not None:
-            added = _search_and_add_left(i, exploded_node, left)
-            if added:
-                return None, None, None, exploded_once
-    return left, right, exploded_node, exploded_once
+def add(left, right):
+    return [left, right]
 
 
-def reduce(raw: str):
-    number = parse(raw)
-    explode(number)
+def _flatten(number, flattened):
+    for idx, n in enumerate(number):
+        if type(n) == list:
+            _flatten(number[idx], flattened)
+        else:
+            flattened.append(n)
+    return flattened
+
+
+def _find_left(flattened, left):
+    idx = flattened.index(left)
+    if idx > 0:
+        return flattened[idx - 1]
+
+
+def _find_right(flattened, right):
+    idx = flattened.index(right)
+    if (idx + 1) < len(flattened):
+        return flattened[idx + 1]
+
+
+def explode(number):
+    return _explode(number, number, 0)
+
+
+def _explode(number, original, depth):
+    for idx, n in enumerate(number):
+        if type(n) == list and depth == 3:
+            flattened = _flatten(original, [])
+            left, right = number[idx]
+
+            left_neighbour = _find_left(flattened, left)
+            right_neighbour = _find_right(flattened, right)
+
+            number[idx] = Wrapper(0)
+
+            if left_neighbour is not None:
+                left_neighbour.add(left)
+
+            if right_neighbour is not None:
+                right_neighbour.add(right)
+
+            return original
+        elif type(n) == list:
+            result = _explode(n, original, depth + 1)
+            if result is not None:
+                return result
+
+
+def split(number):
+    return _split(number, number)
+
+
+def _split(number, original):
+    for idx, n in enumerate(number):
+        if type(n) == Wrapper and n.value > 9:
+            number[idx] = [*n.split()]
+            return original
+        elif type(n) == list:
+            result = _split(n, original)
+            if result is not None:
+                return result
+
+
+def magnitude(number):
+    if type(number) == list and len(number) == 2 and all(type(n) is Wrapper for n in number):
+        return number[0].value * 3 + number[1].value * 2
+    elif type(number[0]) == list and type(number[1]) == Wrapper:
+        return magnitude(number[0]) * 3 + number[1].value * 2
+    elif type(number[0]) == Wrapper and type(number[1]) == list:
+        return number[0].value * 3 + magnitude(number[1])
+    else:
+        return magnitude(number[0]) * 3 + magnitude(number[1]) * 2
+
+
+def reduce(number):
+    while True:
+        has_split = False
+        has_exploded = explode(number) is not None
+        if not has_exploded:
+            has_split = split(number) is not None
+        if not has_exploded and not has_split:
+            break
+    return number
+
+
+def calc_sum(lines):
+    numbers = [wrap(parse(line)) for line in lines]
+    number = numbers[0]
+    for i in range(1, len(numbers)):
+        number = add(number, numbers[i])
+        number = reduce(number)
+    return magnitude(number)
+
+
+def calc_max_sum(lines):
+    magnitudes = []
+    for x in lines:
+        for y in lines:
+            if x != y:
+                n = wrap(parse(x))
+                m = wrap(parse(y))
+                magnitudes.append(magnitude(reduce(add(n, m))))
+
+                o = wrap(parse(x))
+                p = wrap(parse(y))
+                magnitudes.append(magnitude(reduce(add(p, o))))
+
+    return max(magnitudes)
--- day18/snailfish_test.py
+++ day18/snailfish_test.py
@@ -1,34 +1,180 @@
 import unittest
 
-from day18.snailfish import parse, explode
+from common import read_lines
+from day18.snailfish import parse, add, explode, wrap, unwrap, split, magnitude, reduce, calc_sum, calc_max_sum
 
 
 class SnailfishTest(unittest.TestCase):
-    def test_explode(self):
-        # actual = parse("[[[[[9,8],1],2],3],4]")
-        # expected = parse("[[[[0,9],2],3],4]")
-        # explode(actual)
-        # self.assertEqual(expected, actual)
-        #
-        # actual = parse("[7,[6,[5,[4,[3,2]]]]]")
-        # expected = parse("[7,[6,[5,[7,0]]]]")
-        # explode(actual)
-        # self.assertEqual(expected, actual)
-        #
-        # actual = parse("[[6,[5,[4,[3,2]]]],1]")
-        # expected = parse("[[6,[5,[7,0]]],3]")
-        # explode(actual)
-        # self.assertEqual(expected, actual)
 
-        actual = parse("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")
-        expected = parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
-        explode(actual)
-        self.assertEqual(expected, actual)
-        #
-        # actual = parse("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
-        # expected = parse("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
-        # explode(actual)
-        # self.assertEqual(expected, actual)
+    def test_parse(self):
+        self.assertEqual(parse("[1,2]"), [1, 2])
+        self.assertEqual([[1, 2], [[3, 4], 5]], parse("[[1,2],[[3,4],5]]"))
+
+    def test_add(self):
+        result = add([1, 2], [[3, 4], 5])
+        self.assertEqual([[1, 2], [[3, 4], 5]], result)
+
+    def test_wrap(self):
+        result = wrap([[3, 4], 5])
+        self.assertEqual("[[3, 4], 5]", str(result))
+
+    def test_explode_1(self):
+        number = wrap([[[[[9, 8], 1], 2], 3], 4])
+        result = unwrap(explode(number))
+        self.assertEqual([[[[0, 9], 2], 3], 4], result)
+
+    def test_explode_2(self):
+        number = wrap([7, [6, [5, [4, [3, 2]]]]])
+        result = unwrap(explode(number))
+        self.assertEqual([7, [6, [5, [7, 0]]]], result)
+
+    def test_explode_3(self):
+        number = wrap([[6, [5, [4, [3, 2]]]], 1])
+        result = unwrap(explode(number))
+        self.assertEqual([[6, [5, [7, 0]]], 3], result)
+
+    def test_explode_4(self):
+        number = wrap([[3, [2, [1, [7, 3]]]], [6, [5, [4, [3, 2]]]]])
+        result = unwrap(explode(number))
+        self.assertEqual([[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]], result)
+
+    def test_explode_5(self):
+        number = wrap([[3, [2, [8, 0]]], [9, [5, [4, [3, 2]]]]])
+        result = unwrap(explode(number))
+        self.assertEqual([[3, [2, [8, 0]]], [9, [5, [7, 0]]]], result)
+
+    def test_explode_6(self):
+        number = wrap([1, 2])
+        result = unwrap(explode(number))
+        self.assertIsNone(result)
+
+    def test_split_1(self):
+        number = wrap([10, 2])
+        result = unwrap(split(number))
+        self.assertEqual([[5, 5], 2], result)
+
+    def test_split_2(self):
+        number = wrap([11, 2])
+        result = unwrap(split(number))
+        self.assertEqual([[5, 6], 2], result)
+
+    def test_reduce_manually(self):
+        # add
+        number = add(wrap([[[[4, 3], 4], 4], [7, [[8, 4], 9]]]), wrap([1, 1]))
+        self.assertEqual([[[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]], unwrap(number))
+        # explode
+        number = explode(wrap(number))
+        self.assertEqual([[[[0, 7], 4], [7, [[8, 4], 9]]], [1, 1]], unwrap(number))
+        # explode
+        number = explode(wrap(number))
+        self.assertEqual([[[[0, 7], 4], [15, [0, 13]]], [1, 1]], unwrap(number))
+        # split
+        number = split(wrap(number))
+        self.assertEqual([[[[0, 7], 4], [[7, 8], [0, 13]]], [1, 1]], unwrap(number))
+        # split
+        number = split(wrap(number))
+        self.assertEqual([[[[0, 7], 4], [[7, 8], [0, [6, 7]]]], [1, 1]], unwrap(number))
+        # explode
+        number = explode(wrap(number))
+        self.assertEqual([[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]], unwrap(number))
+
+    def test_magnitude_1(self):
+        number = wrap([[9, 1], [1, 9]])
+        result = magnitude(number)
+        self.assertEqual(129, result)
+
+    def test_magnitude_2(self):
+        number = wrap([[1, 2], [[3, 4], 5]])
+        result = magnitude(number)
+        self.assertEqual(143, result)
+
+    def test_magnitude_3(self):
+        number = wrap([[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]])
+        result = magnitude(number)
+        self.assertEqual(1384, result)
+
+    def test_magnitude_4(self):
+        number = wrap([[[[1, 1], [2, 2]], [3, 3]], [4, 4]])
+        result = magnitude(number)
+        self.assertEqual(445, result)
+
+    def test_magnitude_5(self):
+        number = wrap([[[[3, 0], [5, 3]], [4, 4]], [5, 5]])
+        result = magnitude(number)
+        self.assertEqual(791, result)
+
+    def test_magnitude_6(self):
+        number = wrap([[[[5, 0], [7, 4]], [5, 5]], [6, 6]])
+        result = magnitude(number)
+        self.assertEqual(1137, result)
+
+    def test_magnitude_7(self):
+        number = wrap([[[[8, 7], [7, 7]], [[8, 6], [7, 7]]], [[[0, 7], [6, 6]], [8, 7]]])
+        result = magnitude(number)
+        self.assertEqual(3488, result)
+
+    def test_reduce(self):
+        number = add(wrap([[[[4, 3], 4], 4], [7, [[8, 4], 9]]]), wrap([1, 1]))
+        number = reduce(number)
+        self.assertEqual([[[[0, 7], 4], [[7, 8], [6, 0]]], [8, 1]], unwrap(number))
+
+    def test_example_1(self):
+        test_input = [
+            "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]",
+            "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+            "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]",
+            "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]",
+            "[7,[5,[[3,8],[1,4]]]]",
+            "[[2,[2,2]],[8,[8,1]]]",
+            "[2,9]",
+            "[1,[[[9,3],9],[[9,0],[0,7]]]]",
+            "[[[5,[7,4]],7],1]",
+            "[[[[4,2],2],6],[8,7]]"
+        ]
+        sum = calc_sum(test_input)
+        self.assertEqual(3488, sum)
+
+    def test_example_2(self):
+        test_input = [
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+            "[[[[5,4],[7,7]],8],[[8,3],8]]",
+            "[[9,3],[[9,9],[6,[4,9]]]]",
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+        ]
+        sum = calc_sum(test_input)
+        self.assertEqual(4140, sum)
+
+    def test_part_1(self):
+        input = read_lines("day18")
+        sum = calc_sum(input)
+        self.assertEqual(3216, sum)
+
+    def test_example_3(self):
+        test_input = [
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+            "[[[[5,4],[7,7]],8],[[8,3],8]]",
+            "[[9,3],[[9,9],[6,[4,9]]]]",
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+        ]
+        sum = calc_max_sum(test_input)
+        self.assertEqual(3993, sum)
+
+    def test_part_2(self):
+        input = read_lines("day18")
+        sum = calc_max_sum(input)
+        self.assertEqual(4643, sum)
 
 
 if __name__ == '__main__':
